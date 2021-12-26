4be425a8fac5b5a04ff50774413696808dc6a534
Diem Quynh Tran <dquynhcn1.uet@gmail.com>
2021-12-26 13:02:23 +0700

    hashcode:)

--- server/src/controllers/analytics.ts
+++ server/src/controllers/analytics.ts
@@ -7,7 +7,10 @@ export const analyticsController = {
     //[POST] person/analytics/gender
     gender: async (req: Request, res: Response) => {
        try {
-        const code = req.body.code;
+           //console.log(req.params);
+           
+        //const code = req.params._code;
+        const code = req.body.code;        
         let personRepo = await getRepository(Person).createQueryBuilder("person")
         //.addSelect("COUNT()","tong")
         .select("person.gender","gender")
@@ -16,16 +19,19 @@ export const analyticsController = {
         .groupBy("person.gender")
         .getRawMany()
         ;
-        JSON.stringify(personRepo);
-        const result = {
-            "male":String,
-            "female": String
-        }
-        result.male = personRepo[0].tong;
-        result.female = personRepo[1].tong;
+        //console.log(personRepo);
         return res.json({
             status: 200,
-            result: result
+            result: [
+                {
+                    "label":"male",
+                    "count": Number(personRepo[0].tong)
+                },
+                {
+                    "label":"female",
+                    "count":Number(personRepo[1].tong)
+                }
+            ]
         })
        }
        catch (e) {
@@ -101,18 +107,6 @@ export const analyticsController = {
     age: async (req: Request, res: Response) => {
         try {
             const code = req.body.code;
-            const result = {
-                "0-10": "0",
-                "11-20": "0",
-                "21-30": "0",
-                "31-40": "0",
-                "41-50": "0",
-                "51-60": "0",
-                "61-70": "0",
-                "71-80": "0",
-                "81-90": "0",
-                "91-100": "0"
-            }
             
         let age1 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
@@ -120,73 +114,114 @@ export const analyticsController = {
         //.andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>-")
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<11")
         .getRawMany();
-        result["0-10"] = age1[0].num;
+        //result["0-10"] = age1[0].num;
         let age2 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
         .where("person.admincode Like :code", {code: `${code}%`})
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>10")
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<21")
         .getRawMany();
-        result["11-20"] = age2[0].num;
+        //result["11-20"] = age2[0].num;
         let age3 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
         .where("person.admincode Like :code", {code: `${code}%`})
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>20")
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<31")
         .getRawMany();
-        result["21-30"]= age3[0].num;
+        //result["21-30"]= age3[0].num;
         let age4 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
         .where("person.admincode Like :code", {code: `${code}%`})
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>30")
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<41")
         .getRawMany();
-        result["31-40"] = age4[0].num;
+        //result["31-40"] = age4[0].num;
         let age5 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
         .where("person.admincode Like :code", {code: `${code}%`})
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>40")
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<51")
         .getRawMany();
-        result["41-50"] = age5[0].num;
+        //result["41-50"] = age5[0].num;
         let age6 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
         .where("person.admincode Like :code", {code: `${code}%`})
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>50")
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<61")
         .getRawMany();
-        result["51-60"] = age6[0].num;
+        //result["51-60"] = age6[0].num;
         let age7 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
         .where("person.admincode Like :code", {code: `${code}%`})
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>60")
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<71")
         .getRawMany();
-        result["61-70"] = age7[0].num;
+        //result["61-70"] = age7[0].num;
         let age8 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
         .where("person.admincode Like :code", {code: `${code}%`})
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>70")
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<81")
         .getRawMany();
-        result["71-80"] = age8[0].num;
+        //result["71-80"] = age8[0].num;
         let age9 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
         .where("person.admincode Like :code", {code: `${code}%`})
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>80")
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<91")
         .getRawMany();
-        result["81-90"] = age9[0].num;
+        //result["81-90"] = age9[0].num;
         let age10 = await getRepository(Person).createQueryBuilder("person")
         .select("COUNT(person.name)","num")
         .where("person.admincode Like :code", {code: `${code}%`})
         .andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)>90")
         //.andWhere("ROUND(DATEDIFF(CURDATE(),person.birthDay)/365,0)<")
         .getRawMany();
-        result["91-100"] = age10[0].num;
+        //result["91-100"] = age10[0].num;
         return res.json({
             status: 200,
-            result: result
+            result: [
+                {
+                    "label":"0-10",
+                    "value": Number(age1[0].num)
+                },
+                {
+                    "label":"10-20",
+                    "value": Number(age2[0].num)
+                },
+                {
+                    "label":"20-30",
+                    "value": Number(age3[0].num)
+                },
+                {
+                    "label":"30-40",
+                    "value": Number(age4[0].num)
+                },
+                {
+                    "label":"40-50",
+                    "value": Number(age5[0].num)
+                },
+                {
+                    "label":"50-60",
+                    "value": Number(age6[0].num)
+                },
+                {
+                    "label":"60-70",
+                    "value": Number(age7[0].num)
+                },
+                {
+                    "label":"70-80",
+                    "value": Number(age8[0].num)
+                },
+                {
+                    "label":"80-90",
+                    "value": Number(age9[0].num)
+                },
+                {
+                    "label":">90",
+                    "value": Number(age10[0].num)
+                }
+            ]
         })
         }
         catch (e) {

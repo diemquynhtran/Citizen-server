dfc315aed1e653fe67f7cca90aadf561a6bfe66e
Diem Quynh Tran <dquynhcn1.uet@gmail.com>
2021-12-26 22:17:14 +0700

    a

--- server/src/routers/ward.ts
+++ server/src/routers/ward.ts
@@ -5,10 +5,10 @@ import { roleRequire } from "../middlewares/role";
 const express = require("express");
 const router = express.Router();
 
-router.get("/",roleRequire(Role.A1, Role.A2, Role.A3), wardController.getAllwards);
+router.get("/", wardController.getAllwards);
 router.get("/getByA3", wardController.getwardsByRole);
-router.post("/create",roleRequire(Role.A3), wardController.create);
+router.post("/create", wardController.create);
 router.post("/getByDistrict", wardController.getByDistrict);
-router.put("/update",roleRequire(Role.A3), wardController.update);
-router.delete("/delete",roleRequire(Role.A3), wardController.delete);
+router.put("/update", wardController.update);
+router.delete("/delete", wardController.delete);
 export default router;
